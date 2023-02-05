4431da0902c47b78cadf6a069f34253ce560625d
Stalkakuma <stalkakuma@gmail.com>
2023-02-05 21:40:56 +0200

    search styles

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,5 @@
 import { ChangeEventHandler } from "react";
-import { Input, Flex } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement, Flex } from "@chakra-ui/react";
 import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
 
 export const Search = ({
@@ -12,33 +12,29 @@ export const Search = ({
   clearInput: () => void;
 }) => {
   return (
-    <Flex
-      align={"center"}
-      position={"relative"}
-      w={{ md: "40%", base: "100%" }}
-    >
-      <Input
-        aria-label="search countries"
-        type={"text"}
-        bg={"white"}
-        paddingInlineEnd={"40px"}
-        onChange={onChange}
-        placeholder={"Type a country name..."}
-        value={countryName}
-        focusBorderColor={"activeOrange"}
-      />
-      {countryName.length ? (
-        <CloseIcon
-          cursor={"pointer"}
-          position={"absolute"}
-          right={"16px"}
-          zIndex={"docked"}
+    <Flex align="center" position="relative" w={{ md: "40%", base: "100%" }}>
+      <InputGroup>
+        <Input
+          aria-label="search countries"
+          type="text"
+          bg="white"
+          paddingInlineEnd="40px"
+          onChange={onChange}
+          placeholder={"Type a country name..."}
+          value={countryName}
+          focusBorderColor={"activeOrange"}
+        />
+        <InputRightElement
+          cursor="pointer"
+          position="absolute"
+          right="16px"
+          zIndex="docked"
           onClick={clearInput}
           _hover={{ color: "activeOrange" }}
-        />
-      ) : (
-        <SearchIcon position={"absolute"} right={"16px"} zIndex={"docked"} />
-      )}
+        >
+          {countryName.length ? <CloseIcon /> : <SearchIcon />}
+        </InputRightElement>
+      </InputGroup>
     </Flex>
   );
 };
